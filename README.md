a993bf2f5bab534d5594750e2b2e564a2a6066cd
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-06-20 20:22:39 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
-# promet.si-web-scraper
-Web scraper for travel times and vehicle counter from Promet.si
+# Web Scraper for Travel Times and Vehicle Counters from promet.si
+
+## Overview
+This repository contains two web scrapers implemented using Selenium in Python. The scrapers are designed to extract travel times and vehicle count data from promet.si.
+
+## Scrapers
+- <b>Travel Times Scraper</b>: Collects travel time data from some routes in Slovenia.
+
+<img src="travel_times.png" alt="Travel Times" height="400">
+
+<br>
+- <b>Vehicle Counters Scraper</b>: Collects vehicle count data on highways in Slovenia. It can search locations and scroll to take all the data from dynamic table.
+
+<img src="vehicle_counters.png" alt="Vehicle Counters" height="400">
+
+## Prerequisites
+Ensure you have the following installed:
+
+- Python 3.x
+- Selenium
+- Chrome
+
+## Usage
+
+### Travel Times Scraper
+
+Navigate to 'travel_times_scraper' directory:
+```python
+cd travel_times_scraper
+```
+
+Run the scraper:
+```python
+python travel_times_scraper.py
+```
+
+Data saved to csv:
+- datetime
+- latitude
+- longitude
+- location_name
+- minutes
+- traffic_type
+
+### Vehicle Counters Scraper
+
+Navigate to 'vehicle_counters_scraper' directory:
+```python
+cd vehicle_counters_scraper
+```
+
+Run the scraper:
+```python
+python vehicle_counters_scraper.py
+```
+
+Data saved to csv:
+- datetime
+- latitude
+- longitude
+- location_name
+- number_of_vehicles_left_lane
+- number_of_vehicles_right_lane
+- speed_right_lane
+- speed_left_lane
+- spacing_in_sec_right_lane
+- spacing_in_sec_left_lane
+- density_type_right_lane
+- density_type_left_lane
---
+++ travel_times_scraper/travel_times_scraper.py
@@ -0,0 +1,168 @@
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import re
+import os
+import csv
+from datetime import datetime, timezone
+from locations import LOCATIONS
+
+URL = "https://promet.si/sl/potovalni-casi"
+CONTAINER_ID = "potovalni-casi-general-card-26"
+CARD_SELECTOR = "div.card.shadow-sm.moj-promet-item.mt-2"
+LOCATION_SELECTOR = "div.flex-grow-1.fw-bold.text-primary"
+GREEN_BADGE_SELECTOR = "div.badge.badge-green.text-white.text-right.me-1"
+RED_BADGE_SELECTOR = "div.badge.badge-red.text-white.text-right.me-1"
+ORANGE_BADGE_SELECTOR = "div.badge.badge-orange.text-white.text-right.me-1"
+
+def trim_text(text):
+    """
+    Trims text by replacing non-alphanumeric characters with underscores.
+    Example: "Avenue 123, 4th floor" -> "Avenue_123_4th_floor"
+    """
+    # Define a regular expression pattern to find characters that need to be trimmed
+    pattern = r'[^\w\s]'
+    
+    # Replace non-alphanumeric characters with an underscore
+    trimmed_text = re.sub(pattern, '_', text)
+    
+    # Replace multiple underscores with a single underscore
+    trimmed_text = re.sub(r'_+', '_', trimmed_text)
+    
+    # Replace spaces with underscores and remove leading and trailing underscores
+    trimmed_text = trimmed_text.replace(' ', '_').strip('_')
+    
+    # Remove all consecutive underscores
+    return re.sub(r'_{2,}', '_', trimmed_text)
+
+def convert_to_minutes(time_str):
+    """
+    Converts a string representing time to minutes.
+    Examples: "30 min" -> 30, "1 h 30 min" -> 90
+    """
+    if time_str == "Zaprto":
+        return 0
+    
+    parts = time_str.split() # Split the string by space
+
+    if len(parts) == 2: # "30 min" or "1 h"
+        if parts[1] == "min": # "30 min"
+            return int(parts[0])
+        else: # "1 h"
+            return int(parts[0]) * 60
+    elif len(parts) == 4: # "1 h 30 min"
+        hours = int(parts[0])
+        minutes = int(parts[2])
+        return hours * 60 + minutes
+    else:
+        return 0  # Invalid format
+
+def save_to_csv(datetime, location_name, travel_time_min, latitude, longitude, type):
+    path = f"{location_name}"
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    csv_file_path = f"{path}/travel_time_data.csv"
+    csv_exists = os.path.exists(csv_file_path)
+
+    with open(csv_file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+    
+        if not csv_exists:
+            writer.writerow(["datetime", "latitude", "longitude", "location_name", "minutes", "traffic_type"])
+            
+        writer.writerow([datetime, latitude, longitude, location_name, travel_time_min, type])
+
+def scrape():
+    """
+    Scrapers and returns current travel times for each location.
+    """
+    travel_times = {}
+    datetime_utc = datetime.now(timezone.utc)
+    
+    chrome_options = Options()
+    
+    # Run browser in headless mode (without opening a browser window)
+    # chrome_options.add_argument('--headless')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(URL)
+
+    wait = WebDriverWait(driver, 20)
+
+    # Wait for the page to load
+    wait.until(lambda driver: driver.execute_script("return document.readyState") == "complete")
+
+    # Wait for the container to load
+    wait.until(EC.presence_of_element_located((By.ID, CONTAINER_ID)))
+
+    # Find the container
+    #container = driver.find_element(By.ID, CONTAINER_ID)
+
+    cards = driver.find_elements(By.CSS_SELECTOR, CARD_SELECTOR)
+
+    # Extract values from cells in each card
+    for card in cards:
+        location_name = card.find_element(By.CSS_SELECTOR, LOCATION_SELECTOR).text
+        type = "unknown"
+
+        # Check if green badge is present, that means the road is open
+        try:
+            green_badge = card.find_element(By.CSS_SELECTOR, GREEN_BADGE_SELECTOR)
+            if green_badge:
+                time = green_badge.text
+                type = "normal"
+        except: pass
+
+        try:
+            orange_badge = card.find_element(By.CSS_SELECTOR, ORANGE_BADGE_SELECTOR)
+            if orange_badge:
+                time = orange_badge.text
+                type = "medium"
+        except:
+            pass
+
+        # Check if red badge is present, that means the road is closed
+        try:
+            red_badge = card.find_element(By.CSS_SELECTOR, RED_BADGE_SELECTOR)
+            if red_badge:
+                time = red_badge.text
+                type = "high"
+        except: pass
+
+        # Check if the road is closed
+        #road_closed = "Zaprto" in time
+
+        trimmed_location_name = trim_text(location_name)
+        location = LOCATIONS[trimmed_location_name]
+
+        travel_times[trimmed_location_name] = {
+            'datetime': datetime_utc,
+            'location': trimmed_location_name,
+            'time': convert_to_minutes(time),
+            'type': type,
+            'latitude': location.Latitude,
+            'longitude': location.Longitude
+        }
+
+    # Close the browser
+    driver.quit()
+
+    return travel_times
+
+if __name__ == "__main__":
+    travel_times_dictionary = scrape()
+
+    for location, data in travel_times_dictionary.items():
+        save_to_csv(
+            data['datetime'], 
+            trim_text(location),
+            data['time'],
+            data['latitude'],
+            data['longitude'],
+            data['type']
+        )
